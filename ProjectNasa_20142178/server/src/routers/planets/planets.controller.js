99b2f40b01abf1b70d578348b42f4628f7d72b55
Manh-Huy <tranmanhhuy2002@gmail.com>
2023-11-19 16:09:41 +0700

    commit code

--- ProjectNasa_20142178/server/src/routers/planets/planets.controller.js
+++
@@ -1,9 +0,0 @@
-const {getAllPlanets} = require('../../models/planets.model');
-
-function httpgetAllPlanets(rep, res) {
-   return res.status(200).json(getAllPlanets());
-}
-
-module.exports = {
-    httpgetAllPlanets,
-}
--- ProjectNasa_20142178/server/src/routers/planets/planets.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const {httpgetAllPlanets} = require('./planets.controller')
-
-const planetsRouter = express.Router();
-
-planetsRouter.get('/',httpgetAllPlanets);
-
-module.exports = planetsRouter;
